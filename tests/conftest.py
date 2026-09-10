dd714ed4a0f10f499faea9bfaafd9f2763656fcc
Dogi <dogisocks@yandex.ru>
2026-09-10 22:03:18 +0300

    fix: ci tests req add passlib

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,3 @@
-import asyncio
-import pytest
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
